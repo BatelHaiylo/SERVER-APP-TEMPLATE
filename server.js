bb2d070f99a273fbc00cf8caf026194771dfba87
Batel Haiylo <inpo686@gmail.com>
2022-11-13 13:20:20 +0200

    ready for use

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 8080
+
+app.use(express.json({extended: true}));
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.get('/', (req,res)=>{
+    res.send("server is working")
+})
+
+app.listen(port, ()=>{
+    console.log(`server is listening in port ${port}`)
+})
